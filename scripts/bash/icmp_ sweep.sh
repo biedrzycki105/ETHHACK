1aab1a8434580856cd3245a29fd124979396d432
biedrzycki105 <biedrzycki105@gmail.com>
2024-05-02 23:08:43 -0400

    .

--- scripts/bash/icmp_ sweep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-outfile=../output/icmp_sweep-$(date +%d-%m-%y-%H%M%S).txt
-
-for ip in $(seq 2 50); do 
-echo "Pinging $1.$ip..."
-ping -c 1 $1.$ip | grep "64 bytes" | awk '{print $4}' | cut -d ":" -f1 >> $outfile
-done
